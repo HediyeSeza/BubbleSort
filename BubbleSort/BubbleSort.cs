dcb2cfbba4e5ad3bc9c81109c093442f00f44d41
sezahedi <sezahedi13@gmail.com>
2022-05-29 21:22:55 +0430

    Add project files.

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace BubbleSort.Library
+{
+    public class BubbleSort
+    {
+
+        public static int[] Sort(int[] array)
+        {
+            int temp;
+            bool isSortCompleted = true;
+            for (int i = 0; i < array.Length - 1 && isSortCompleted; i++)
+            {
+                isSortCompleted = false;
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        isSortCompleted = true;
+                    }
+                }
+            }
+            return array.ToArray();
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int[] A = { 2, 8, 6, 4, 3 };
+            Console.WriteLine("UnSort Array :");
+            PrintArray(A);
+            int[] rs = BubbleSort.Library.BubbleSort.Sort(A);
+            Console.WriteLine("\n\nSorted Array :");
+            PrintArray(rs);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+        }
+
+    }
+}
